0dcbed0b2cca8ecd1383a9f4105c8faa00e7a45d
oguzhan.erdem <og2!3rdm>
2021-01-01 19:29:23 +0300

    dataprovider

--- src/test/java/Case/SearchCase.java
+++ src/test/java/Case/SearchCase.java
@@ -3,19 +3,20 @@ package Case;
 import Tests.SearchTest;
 import org.testng.annotations.Test;
 
-public class SearchCase {
+public class SearchCase extends BaseCase {
     SearchTest searchTest = new SearchTest("d1b66b77", "http://www.omdbapi.com/");
 
-    @Test
-    public void SearchCases() {
+
+    @Test(dataProvider = "dataForSearchGetCase")
+    public void searchGetCases(String searchWord, String expectedResult) {
         searchTest.check_StatusCode200();
-        searchTest.check_IsParamExisting("Title", "Harry Potter"); //check parameter
+        searchTest.check_IsParamExisting("Title", searchWord); //check parameter
         // existence with Harry Potter Search
-        searchTest.check_IsParamExisting("Year", "Harry Potter");
-        searchTest.check_IsParamExisting("Released", "Harry Potter");
-        searchTest.check_MovieExit("Harry Potter", "Harry Potter and the Deathly Hallows: Part 2");//check
+        searchTest.check_IsParamExisting("Year", searchWord);
+        searchTest.check_IsParamExisting("Released", searchWord);
+        searchTest.check_MovieExit(searchWord, expectedResult);//check
         // movie exist
-        searchTest.check_IdOfMovie("Harry Potter and the Deathly Hallows: Part 2"); //checking title result and id result equal
+        searchTest.check_IdOfMovie(expectedResult); //checking title result and id result equal
     }
 
 }
