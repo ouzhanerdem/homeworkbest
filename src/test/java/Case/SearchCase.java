2614203a18cc49f12c42a0ef7149649a7a66e8bd
oguzhan.erdem <og2!3rdm>
2021-01-01 16:45:31 +0300

    refactor

--- src/test/java/Case/SearchCase.java
+++ src/test/java/Case/SearchCase.java
@@ -8,9 +8,9 @@ public class SearchCase {
     @Test
     public void SearchCases(){
         searchTest.check_StatusCode200();
-        searchTest.check_IsParamExisting("Title");
-        searchTest.check_IsParamExisting("Year");
-        searchTest.check_IsParamExisting("Released");
+        searchTest.check_IsParamExisting("Title", "Harry Potter");
+        searchTest.check_IsParamExisting("Year", "Harry Potter");
+        searchTest.check_IsParamExisting("Released","Harry Potter");
         searchTest.check_MovieExit("Harry Potter","Harry Potter and the Deathly Hallows: Part 2");
         searchTest.check_IdOfMovie("Harry Potter and the Deathly Hallows: Part 2");
     }
